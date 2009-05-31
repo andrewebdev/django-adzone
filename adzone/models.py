b36531d7ce23ca8c7ea65c365921e6e15e891586
Andre Engelbrecht <andre@teh-node.co.za>
2009-05-31 21:15:13 +0100

    Started the ad model

--- adzone/models.py
+++ adzone/models.py
@@ -6,3 +6,24 @@
 # If this script is distributed, it must be accompanied by the Licence
 
 from django.db import models
+
+def datetimenow():
+    """Just s quick function to return the current date and time"""
+    from datetime import datetime
+    return datetime.datetime.now()
+
+class Ad(models.Model):
+    """ Our standard Ad Model
+    """
+    title = models.CharField(max_length=255,
+            help_text='A short title for the ad')
+    content = models.TextField(
+            help_text='The text based content for the ad')
+    url = models.URLField(verify_exists=True,
+            help_text='The url to which the advert points')
+    advertiser = models.ForeignKey('Advertiser')
+    words = models.TextField(
+            help_text='a couple of words to describe the advert')
+    enabled = models.BooleanField(default=False)
+    since = models.DateTimeField(default=datetimenow())
+    updated = models.DateTimeField()
