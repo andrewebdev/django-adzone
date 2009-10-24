d6475e8ad9039954c095aa695ec23203c6de5ae4
Andre Engelbrecht <andre@teh-node.co.za>
2009-10-24 16:25:46 +0100

    Admin displays optimized a bit

--- src/adzone/admin.py
+++ src/adzone/admin.py
@@ -6,40 +6,43 @@
 # If this script is distributed, it must be accompanied by the Licence
 
 from django.contrib import admin
-from adzone.models import Advertiser, AdCategory, AdZone, TextAd, BannerAd, FlashAd, AdClick, AdImpression
+from adzone.models import *
 
 class AdvertiserAdmin(admin.ModelAdmin):
     search_fields = ['company_name', 'website']
+    list_display = ['company_name', 'website', 'user']
+
+class AdCategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ['title']}
+    list_display = ['title', 'slug']
 
 class AdZoneAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'description']
 
-class TextAdAdmin(admin.ModelAdmin):
-    list_display = ['title', 'url', 'advertiser', 'updated', 'enabled']
-    list_filter = ['updated', 'enabled']
+class AdBaseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'url', 'advertiser', 'since', 'updated', 'enabled']
+    list_filter = ['updated', 'enabled', 'since', 'updated']
+    search_fields = ['title', 'url']
+
+class TextAdAdmin(AdBaseAdmin):
     search_fields = ['title', 'url', 'content']
 
-class BannerAdAdmin(admin.ModelAdmin):
-    list_display = ['title', 'url', 'advertiser', 'updated', 'enabled']
-    list_filter = ['updated', 'enabled']
-    search_fields = ['title', 'url']
-
-class FlashAdAdmin(admin.ModelAdmin):
-    list_display = ['title', 'url', 'advertiser', 'updated', 'enabled']
-    list_filter = ['updated', 'enabled']
-    search_fields = ['title', 'url']
-
 class AdClickAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['ad', 'source_ip']
+    list_display = ['ad', 'click_date', 'source_ip']
+    list_filter = ['click_date']
+    date_hierarchy = 'click_date'
 
 class AdImpressionAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['ad', 'source_ip']
+    list_display = ['ad', 'impression_date', 'source_ip']
+    list_filter = ['impression_date']
+    date_hierarchy = 'impression_date'
 
 admin.site.register(Advertiser, AdvertiserAdmin)
-admin.site.register(AdCategory)
+admin.site.register(AdCategory, AdCategoryAdmin)
 admin.site.register(AdZone, AdZoneAdmin)
 admin.site.register(TextAd, TextAdAdmin)
-admin.site.register(BannerAd, BannerAdAdmin)
-admin.site.register(FlashAd, FlashAdAdmin)
+admin.site.register(BannerAd, AdBaseAdmin)
 admin.site.register(AdClick, AdClickAdmin)
 admin.site.register(AdImpression, AdImpressionAdmin)
