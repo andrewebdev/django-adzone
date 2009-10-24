886f3424175021ecf3cf66242820ed1cba060a88
Andre Engelbrecht <andre@teh-node.co.za>
2009-10-24 11:35:12 +0100

    Eggified django-adzone. Also removed the uneccesary Project folder

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django-adzone",
+    version="0.2",
+    url="http://github.com/winterweaver/django-adzone",
+    description="A django app to manage adverts according to zones on a website.",
+    author="Andre Engelbrecht",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'setuptools',
+    ],
+)
